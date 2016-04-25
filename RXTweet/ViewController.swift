6425deee810b353462970912dbd41d954b3ccdf8
Anak Mirasing <thaihooligan@hotmail.com>
2016-04-25 16:00:17 +0700

    implement all stream to ViewController

--- RXTweet/ViewController.swift
+++ RXTweet/ViewController.swift
@@ -7,21 +7,109 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import ObjectMapper
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var refreshButton: UIButton!
     
+    var disposeBag = DisposeBag()
+    var users:[User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // setup
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: SETUP
+    func setup() {
+        
+        // TableView
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerNib(UINib(nibName: UserCell.identifier,bundle: nil), forCellReuseIdentifier: UserCell.identifier)
+        
+        // Stream
+        let refreshClickStream = refreshButton.rx_tap.asObservable()
+        
+        let requestStream = refreshClickStream
+            .startWith(Void())
+            .map { (_) -> Void in
+                self.showLoadingHUD()
+            }
+            .map { (_) -> ApiService in
+                return .GetUsersWithRandomOffset
+            }
+        
+        let responseStream = requestStream.flatMap { (service: ApiService) -> Observable<AnyObject> in
+            return GitProvider.request(service).mapJSON()
+            }
+        
+        responseStream
+            .showErrorHUD()
+            .subscribeNext { responseJSON in
+                
+                guard let usersArray = Mapper<User>().mapArray(responseJSON) else { return }
+                self.users = usersArray
+                
+                self.tableView.reloadData()
+                self.hideLoadingHUD()
+            }.addDisposableTo(disposeBag)
+        
+        // Example
+        /*
+        responseStream
+            .subscribe { event in
+            switch event {
+                case .Next(let responseJson):
+                    print(responseJson)
+                break
+                case .Error(let error):
+                    print("Error : \(error)")
+                break
+                case .Completed:
+                    print("responseStream : Complete")
+                break
+            }
+        }.addDisposableTo(disposeBag)
+        */
+        
+    }
+    
+    // MARK: TableView DataSource & Delegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UserCell.height
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(UserCell.identifier, forIndexPath: indexPath)
+        cell.textLabel?.text = users[indexPath.row].username
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
 
 
 }
