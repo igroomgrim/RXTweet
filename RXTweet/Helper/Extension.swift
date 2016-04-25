d4c4a97373f24ba585eb5eaaca1b8dc98db2cca3
Anak Mirasing <thaihooligan@hotmail.com>
2016-04-25 15:24:33 +0700

    add custom extension for show error and display/hide loading HUD

---
+++ RXTweet/Helper/Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Extension.swift
+//  RXTweet
+//
+//  Created by Anak Mirasing on 4/25/16.
+//  Copyright © 2016 VINTX. All rights reserved.
+//
+
+import Foundation
+import SVProgressHUD
+import RxSwift
+import Moya
+
+extension Observable {
+    func showErrorHUD() -> Observable<Element> {
+        return self.doOn { event in
+            switch event {
+            case .Error(let e):
+                // Unwrap underlying error
+                guard let error = e as? Moya.Error else { throw e }
+                guard case .StatusCode(let response) = error else { throw e }
+                
+                SVProgressHUD.showErrorWithStatus("Error \(response.statusCode)")
+                
+            default: break
+            }
+        }
+    }
+}
+
+extension ViewController {
+    
+    func showLoadingHUD() {
+        SVProgressHUD.show()
+    }
+    
+    func hideLoadingHUD() {
+        SVProgressHUD.dismiss()
+    }
+}
